229668177cc0a2cf98a0e0d2f822b7e6a5ce756f
Unknown <blossomperez18@gmail.com>
2020-10-27 07:57:12 -0500

    Correct syntax error

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,8 +1,8 @@
--- DROP DATABASE IF EXISTS codeup_test_db;
--- CREATE DATABASE codeup_test_db;
+DROP DATABASE IF EXISTS codeup_test_db;
+CREATE DATABASE codeup_test_db;
 USE codeup_test_db;
 
---DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
