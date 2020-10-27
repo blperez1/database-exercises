fb3e4e61cc209741fe811de73218b692d186cc6e
Unknown <blossomperez18@gmail.com>
2020-10-27 07:56:21 -0500

    Add more albums to tables

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,2 +1,7 @@
 INSERT INTO codeup_test_db.albums(artist, album_name, release_date, sales, genre)
-VALUES("Michael", "Thriller", "1976", "24.4", "post-disco");
+VALUES("Michael Jackson", "Thriller", "1976", "24.4", "post-disco"),
+    ("AC/DC", "Back in Black", "1980", "29.4", "Hard rock"),
+    ("Pink Floyd", "The Dark Side of the Moon", "1973", "24.4", "Progressive rock"),
+    ("Led Zeppelin", "Led Zeppelin IV", "1971", "29.0", "Hard rock"),
+    ("Alanis Morissette", "Jagged Little Pill", "1995", "24.4", "Alternative rock"),
+    ("Nirvana", "Nevermind", "1991", "16.7", "Grunge");
