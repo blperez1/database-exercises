16007b6eae679d3a5aa93a9a55939604f46d6211
Unknown <blossomperez18@gmail.com>
2020-10-26 23:24:58 -0500

    Uncomment code

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,15 +1,15 @@
-DROP DATABASE IF EXISTS codeup_test_db;
-CREATE DATABASE codeup_test_db;
+-- DROP DATABASE IF EXISTS codeup_test_db;
+-- CREATE DATABASE codeup_test_db;
 USE codeup_test_db;
 
-DROP TABLE IF EXISTS albums;
+--DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(128) NOT NULL,
-  name VARCHAR(128) NOT NULL,
+  artist VARCHAR(50) NOT NULL,
+  album_name VARCHAR(50) NOT NULL,
   release_date YEAR,
   sales DECIMAL (9, 5) ,
   genre VARCHAR(50),
-  PRIMARY KEY(id)
+  PRIMARY KEY(`id`)
 );
