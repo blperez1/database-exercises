712f046e3c7092d909269421d3b8166d42339fb2
Unknown <blossomperez18@gmail.com>
2020-10-28 16:02:44 -0500

    Correct syntax error

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,13 @@
-DROP DATABASE IF EXISTS codeup_test_db;
-CREATE DATABASE codeup_test_db;
-USE codeup_test_db;
+-- DROP DATABASE IF EXISTS codeup_test_db;
+-- CREATE DATABASE codeup_test_db;
+-- USE codeup_test_db;
 
-DROP TABLE IF EXISTS albums;
+-- DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(50) NOT NULL,
-  album_name VARCHAR(50) NOT NULL,
+  artist VARCHAR(150) NOT NULL,
+  album_name VARCHAR(150) NOT NULL,
   release_date YEAR,
   sales DECIMAL (9, 5) ,
   genre VARCHAR(50),
