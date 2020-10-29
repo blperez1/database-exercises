5b7d61bf5eaa7697641ec71b272576af1d713ac2
Unknown <blossomperez18@gmail.com>
2020-10-28 19:01:26 -0500

    Complete limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,22 @@
+USE employees;
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT *
+FROM employees
+WHERE birth_date like '%-12-25'
+ORDER BY birth_date DESC
+    AND hire_date BETWEEN 1990 AND 1999
+ORDER BY hire_date
+LIMIT 5;
+
+SELECT *
+FROM employees
+WHERE birth_date like '%-12-25'
+ORDER BY birth_date DESC
+    AND hire_date BETWEEN 1990 AND 1999
+ORDER BY hire_date
+LIMIT 5 OFFSET 50;
